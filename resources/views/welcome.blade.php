56fda9f9eec196cf17798d948779881928f7c154
tiandiesel <guardor@163.com>
2018-01-31 02:03:59 +0000

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-- tian system
+                    Laravel-- tian system1
                 </div>
 
                 <div class="links">
